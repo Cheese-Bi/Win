9e92fa4105256ddb90c06f8cd5aebba1b3db7f0c
Cheese-Bi <66460858+Cheese-Bi@users.noreply.github.com>
2020-07-02 21:14:48 +0700

    Update and rename index.html to index.php

--- shop copy/index.php
+++ shop copy/index.php
@@ -14,7 +14,10 @@
                       <link href="css/style.css" rel="stylesheet">
                       <!--[if lt IE 9]><script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script><script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script><![endif]-->
                    </head>
+               
                    <body id="home">
+                     <?php echo'<p></p>'; ?>
+                      
                       <div class="wrapper">
                          <div class="header">
                             <div class="container">
